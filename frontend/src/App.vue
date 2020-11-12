40f1a5a51b251ffee8f07ca68d35cffca1ef48b9
Andy H. Jung <commaniakr@gmail.com>
2020-11-12 10:51:27 +0900

    feat: 프로젝트 초기 설정 및 메인 페이지 설정

---
+++ frontend/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img class="logo" src="./assets/splash.jpg" alt="Hothead Factory">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+body {
+  background-color: black;
+}
+#app {
+  font-family: 'Noto Sans CJK', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.logo {
+  width: 100%
+}
+
+@media (min-width: 800px) {
+  .logo {
+    width: 800px
+  }
+}
+</style>
---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="hello">
+    <h2>Available Services:</h2>
+    <div class="button-combo">
+      <button type="button" class="btn btn-dark" onclick="location.href='https://hotheadfactory.com'">blog</button>
+      <button type="button" class="btn btn-dark" onclick="location.href='https://www.youtube.com/channel/UCMFvYIfeIdZeEWR0hthBOXQ'">YouTube</button>
+      <button type="button" class="btn btn-dark" onclick="location.href='https://www.instagram.com/hotheadfactory'">Instagram</button>
+    </div>
+    <div class="button-single">
+      <button type="button" class="btn btn-dark" onclick="location.href='https://hotheadfactory.com'">GitHub</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: white;
+}
+.hello {
+  color: white;
+}
+
+.btn {
+  padding: 10px;
+  font-size: 15px;
+  border-radius: 5px;
+  border-width: 0;
+}
+
+.btn-dark {
+  background-color: #343A40;
+  color: white;
+}
+
+.btn-dark:hover {
+  background-color: gray;
+}
+
+.btn-dark:active {
+  background-color: #343A40;
+}
+
+.button-combo, .button-single {
+  width: 280px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 5px
+}
+
+@media (max-width: 300px) {
+  .button-combo, .button-single {
+    width: 100%;
+  }
+
+  .button-combo>.btn {
+    width: 100% !important;
+    margin-bottom: 5px;
+  }
+}
+
+.button-combo>.btn {
+  width: 32%
+}
+
+.button-single>.btn {
+  width: 100%
+}
+</style>
